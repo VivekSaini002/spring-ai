be5885aa3e5bc74e194bc1ac1384a9211b6ce8d2
Vivek_Saini <viveksaini15702@gmail.com>
2026-06-30 12:17:40 +0530

    Using the JdbcChatMemoryRepository to store the chat message in our MySQL database

--- MemoryApp/src/main/java/com/ai/config/AiConfig.java
+++ MemoryApp/src/main/java/com/ai/config/AiConfig.java
@@ -7,11 +7,21 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(10)
+                .build();
+    }
 
     private Logger logger = LoggerFactory.getLogger(AiConfig.class);
 
